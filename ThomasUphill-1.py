69d9c546f23210829232ff0d7139054a2d3cfe49
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-05-15 20:35:33 -0700

    Update ThomasUphill-1.py

--- ThomasUphill-1.py
+++ ThomasUphill-1.py
@@ -8,7 +8,9 @@ Packt Publishing, 2016
 
 `===Using Librarian====
 
-***Librarian is a bundler for Ruby. It handles dependency checking for you. The project for using Librarian with Puppet is called librarian-puppet and is available at http://rubygems.org/gems/librarian-puppet. To install librarian-puppet, we'll use RubyGems 
+***Librarian is a bundler for Ruby. It handles dependency checking for you. The project for using Librarian with Puppet
+is called librarian-puppet and is available at http://rubygems.org/gems/librarian-puppet. 
+ To install librarian-puppet,we'll use RubyGems 
 
 [root@stand ~]# puppet resource package librarian-puppet ensure=installed provider=gem
 Notice: /Package[librarian-puppet]/ensure: created
@@ -28,4 +30,22 @@ You can override the location of Puppet Forge using a forge line as well.
 forge "http://forge.puppetlabs.com"
 mod 'puppetlabs/puppetdb', '5.0.0'
 mod 'puppetlabs/stdlib', '4.9.0'
+
+==> create a new public directory in /tmp/public4 and include the Puppetfile in that directory, as shown here:
+
+[git@stand ~]$ cd /tmp
+[git@stand tmp]$ mkdir public4 && cd public4
+[git@stand public4]$ cat<<EOF>Puppetfile
+> forge "https://forgeapi.puppetlabs.com"
+>mod 'puppetlabs/puppetdb', '5.0.0'
+>mod 'puppetlabs/stdlib', '4.9.0'
+> EOF
+==>Next, we'll tell librarian-puppet to install everything we've listed in the Puppetfile as follows:
+
+[git@stand public4]$ librarian-puppet update
+[git@stand public4]$ ls
+modules  Puppetfile  Puppetfile.lock
+***The Puppetfile.lock file is a file used by librarian-puppet to keep track of installed versions and dependencies;
+
+
 
